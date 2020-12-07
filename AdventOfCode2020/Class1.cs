fa772a0174cb13209c816bd5cd01fcd4df5d013f
Chris Bacon <cbacon@mavenlink.com>
2020-12-07 15:06:25 -0700

    initial project setup

---
+++ AdventOfCode2020/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+  public class TestMe
+  {
+    public int Setup()
+    {
+      return 2020;
+    }
+  }
+}
---
+++ AdventTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using AdventOfCode2020;
+using Xunit;
+
+namespace AdventTest
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void TestSetup()
+    {
+      Assert.Equal(2020, new TestMe().Setup());
+    }
+  }
+}
