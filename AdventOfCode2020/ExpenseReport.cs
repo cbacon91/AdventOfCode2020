0c7eeabb5c4b6e7a1a3c585de28b1814e6b788cd
Chris Bacon <cbacon@mavenlink.com>
2020-12-07 21:15:51 -0700

    technically passing part 2 with a weird solution

--- AdventOfCode2020/ExpenseReport.cs
+++ AdventOfCode2020/ExpenseReport.cs
@@ -7,6 +7,7 @@ namespace AdventOfCode2020
   public class ExpenseReport : Solver
   {
     public IEnumerable<int> Expenses { get; }
+    private int TARGET = 2020;
 
     public ExpenseReport(IEnumerable<int> expenses)
     {
@@ -18,19 +19,38 @@ namespace AdventOfCode2020
       Expenses = ReadInput("expense_report");
     }
 
-    public (int, int) ExpensesThatSumTo(int target)
+    public int[] SummableExpenses2()
     {
       foreach (var cost in Expenses)
       {
-        var complement = target - cost;
+        var complement = TARGET - cost;
 
         if (Expenses.Contains(complement))
         {
-          return (cost, complement);
+          return new int[] { cost, complement };
         }
       }
 
       throw new Exception("There is no sum to the selected value.");
     }
+
+    public int[] SummableExpenses3()
+    {
+      foreach (var cost in Expenses)
+      {
+        foreach(var complement in Expenses.Except(new int[] { cost }))
+        {
+          var tertiary = TARGET - cost - complement;
+
+          if (Expenses.Contains(tertiary))
+          {
+            return new int[] { cost, complement, tertiary };
+          }
+        }
+      }
+
+      throw new Exception("There is no sum to the selected value.");
+    }
+
   }
 }
--- AdventTest/ExpenseReportTest.cs
+++ AdventTest/ExpenseReportTest.cs
@@ -9,15 +9,29 @@ namespace AdventTest
     [Fact]
     public void TestExpenseSum()
     {
-      var subject = new ExpenseReport(new int[] { 20, 2000 });
-      Assert.Equal((20, 2000), subject.ExpensesThatSumTo(2020));
+      var subject = new ExpenseReport(new int[] { 1721, 979, 366, 299, 675, 1456 });
+      Assert.Equal(new int[] { 1721, 299 }, subject.SummableExpenses2());
+    }
+
+    [Fact]
+    public void TestExpenseSum3()
+    {
+      var subject = new ExpenseReport(new int[] { 1721, 979, 366, 299, 675, 1456 });
+      Assert.Equal(new int[] { 979, 366, 675 }, subject.SummableExpenses3());
     }
 
     [Fact]
     public void TestExpenseSumWithInput()
     {
       var subject = new ExpenseReport();
-      Assert.Equal((1632, 388), subject.ExpensesThatSumTo(2020));
+      Assert.Equal(new int[] { 1632, 388 }, subject.SummableExpenses2());
+    }
+
+    [Fact]
+    public void TestExpenseSumWithInput3()
+    {
+      var subject = new ExpenseReport();
+      Assert.Equal(new int[] { 1607, 196, 217 }, subject.SummableExpenses3());
     }
   }
 }
